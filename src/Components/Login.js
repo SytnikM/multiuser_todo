89182d260c5903027e2557353c550fa5c4ad81b9
Maksim <maksim@Air-Maksim.center-ris.ru>
2022-11-14 18:01:29 +0500

    Add error message and filter password chars in logIn form

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -5,22 +5,31 @@ export default class Login extends React.Component {
         activeUserName: "",
         activeUserPass: "",
         error: "",
-        users: this.props.users
+        users: this.props.users,
+        pass: ""
     }
 
+    // Запись введеного имени пользователя для входа
     onChangeUser = (event) => {
         this.setState({
             ...this.state,
             activeUserName: event.target.value
         })
     }
+    // Запись введеного пароля для входа
     onChangePass = (event) => {
+        let regexp = /[^a-zа-яё,._\-\/=\!\?0-9\s]/gi;
+        let value =event.target.value;
+        value = value.replace(/^\s/,'');
+        value = value.replace(/ /,'');
+        value = value.replace(regexp,'');
         this.setState({
             ...this.state,
-            activeUserPass: event.target.value
-        })
+            activeUserPass: event.target.value,
+            pass: value
+        });
     }
-
+    // Проверка введенных данных
     login = () => {
         this.state.users.map((user)=>{
             if(user.name === this.state.activeUserName){
@@ -30,32 +39,52 @@ export default class Login extends React.Component {
                 }
                 else{
                     console.error("Password is not correct")
+                    this.setState({
+                        ...this.state,
+                        error: "Введен не верный пароль"
+                    })
                 }
             }
         })
         console.log(this.state.activeUserName+"---"+this.state.activeUserPass)
+    }
+    // Добавление возможности входа по нажатию Enter на input'e
+    keyPress = (event) =>{
+        if(event.keyCode === 13){
+            this.login()
+        }
     }
 
     render() {
         return(
             <div>
                 <div className="Login">
-                    <h3>{this.state.error}</h3>
+                    {
+                        this.state.error
+                            ? <h3>{this.state.error}</h3>
+                            : null
+                    }
                     <select 
                         className="username"
                         placeholder="Выберите пользователя"
                         onChange={this.onChangeUser}
                     >
+                        <option disabled selected>Выберите пользователя</option>
                         {this.props.users.map((user) =>{
                             return(
                                 <option>{user.name}</option>
                             )
                         })}
                     </select>
-                    <textarea
-                        type="text"
+                    <input 
+                        value={this.state.pass}
+                        type="password"
                         placeholder="Пароль"
                         onChange={this.onChangePass}
+                        // onChange={this.inputCheck}
+                        onInput={this.inputCheck}
+                        maxLength={20}
+                        onKeyDown={this.keyPress}
                     />
                     <button onClick={this.login}>Войти</button>
                 </div>
